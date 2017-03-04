943e4e897a0d011b0d998bda3d35b507d1c553af
Lautaro <lautarocastillo.93@gmail.com>
2017-03-04 06:10:39 -0300

    array questions

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,16 +1,50 @@
 class GameController < ApplicationController
+  
   def play
-  	@user = current_user
-  	@questions = Question.all
-    
-  	@order = rand(1..3) 
+    @user = current_user
+    @questions = Question.where(level: @user.level).to_a
+    @question = @questions.sample
+    @order = rand(1..3) 
   end
 
+
   def answer
-  	if params[:answer].present?
-  		answer = params[:answer]
-  	  p answer
-  	end
+    @questions.delete(@question)
+    @pflu = 0
+    if params[:answer].present?
+      answer = params[:answer]
+        if answer == "a"
+          @pflu += 1
+            if @pflu == 3
+              level_up
+              @plf = 0
+            end
+        else
+          lose_life
+            
+        end
+    end
+    
+  end
+
+
+  def lose_life
+    if @user.life > 0
+      ull = @user
+      ull.life-=1
+      ull.save
+    else 
+      game_over
+    end
+  end
+
+  def game_over
+  end
+
+  def level_up
+    ulu = @user
+    ulu.level += 1
+    ulu.save
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,13 @@ Rails.application.routes.draw do
   post 'game/answer'
 
 
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+	resources :users do
+		 post 'game/answer'
+	end		
+
   resources :questions
+
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'slangs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
