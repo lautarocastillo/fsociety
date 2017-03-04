bb1f6a7d68cc6d010583be486849b5f024affbe1
Lautaro <lautarocastillo.93@gmail.com>
2017-03-03 21:58:51 -0300

    add devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+
+User.create(email: 'admin@com', password:'123456')
