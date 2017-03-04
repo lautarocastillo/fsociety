f0938bd0ce8e0c7e04335ca55266baece2a17e72
jpablonunezr <jpablo.nunezr@gmail.com>
2017-03-04 08:29:34 -0300

    facebook css, css mas

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   resources :questions
 
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+
   root 'slangs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
